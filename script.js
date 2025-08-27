d5e9af5efcdb7c25f1c93314c3df2b0387e91aaf
1SosukeAizen1 <aizengoku1@gmail.com>
2025-08-27 11:07:34 -0500

    ff

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Hardcoded default screen types
-var screenType = "Drive1/2"; // Main menu
-var secondaryScreenType = "Drive1/2"; // Secondary menu
+var screenType = "drive2"; // Main menu
+var secondaryScreenType = "drive2/2"; // Secondary menu
 
 
 // Function to load a menu dynamically
